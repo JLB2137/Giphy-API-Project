6171bf767b09eae3b30f844aa595cc5d35e1cedd
Jeremee <jeremee.bornstein@gmail.com>
2021-10-26 22:09:05 -0400

    added text and submit button as well as functionality for both functions in JS file

--- js/script.js
+++ js/script.js
@@ -1,43 +1,66 @@
 //alert('JS Working')
 
 const $img = $('img')
-const $submit = $('input')
+const $shuffle = $('input[value="Shuffle"]')
+const $userInput = $('input[type="text"]')
+const $submit = $('input[value="Submit"]')
 const URL = 'https://api.giphy.com/v1/gifs/search'
 const API_KEY = '?api_key=hvGei9QgKHO2wGpIseEMtFaRyyd0dPN2'
-let resultsCounter = 0
 let searchInput = 'cheese'
+let promise = ''
 //object for interating with the gif setup
 const gif = {
     search: `&q=${searchInput}`,
-    imageSource: ''
+    imageSource: '',
+    resultsCounter: 0
 }
 
-let promise = $.ajax(`${URL}${API_KEY}${gif.search}`)
 
 
+function giphyAPI() {
+    
+    promise = $.ajax(`${URL}${API_KEY}${gif.search}`)
 
-promise.then(function(response) {
-    //console.log(response.data[0].url)
-    $img.attr('src',`${gif.imageSource}`)
-    submit(response)
-    //console.log(imageValue)
-    //console.log(response)
-    //$img.attr('src',`${imageValue}`)
-}, function(error) {
-    console.log(error)
-})
+    promise.then(function(response) {
+        //set the image source equal to the response
+        gif.imageSource = response.data[gif.resultsCounter].images.original.url
+        //console.log(response.data[0].url)
+        $img.attr('src',`${gif.imageSource}`)
+        //if you hit the shuffle button change the gif to the next in the stack that was returned
+        shuffle(response)
 
-function submit(response) {
-    $submit.on("click", function(evt) {
+    }, function(error) {
+        console.log(error)
+    })
+}
+
+
+function shuffle(response) {
+    $shuffle.on("click", function(evt) {
         //prevent default behavior
         evt.preventDefault()
         //set the image source equal to the response
-        gif.imageSource = response.data[resultsCounter].images.original.url
+        gif.imageSource = response.data[gif.resultsCounter].images.original.url
         //update the counter to move to the next gif in the array
-        resultsCounter++
+        gif.resultsCounter++
         //update the image with the next gif in the array
         $img.attr('src',`${gif.imageSource}`)
-
     })
 
 }
+
+function updateSearch(input) {
+    //update the searchInput variable to what is the input
+    searchInput = input
+    //update the total gif search string to include the new search
+    gif.search = `&q=${searchInput}`
+}
+
+$submit.on("click", function(evt) {
+    updateSearch($userInput.val())
+    //reset the results counter
+    gif.resultsCounter = 0
+    giphyAPI()
+})
+
+
